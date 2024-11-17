5a05f5d1c80ca163fe33cfedc11033a1a8d905c9
hibaallhhassan <s1023027@uomustansiriyah.edu.iq>
2024-11-17 20:53:13 +0300

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp69
+{
+    //Q1
+    public class Student
+    {
+        public string id;
+        public string name;
+        public double mark;
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            Console.WriteLine("Enter the number of students (5-10): ");
+            int numStudents = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numStudents; i++)
+            {
+                Console.WriteLine($"\nStudent {i + 1}:");
+                Console.Write("Enter ID: ");
+                string id = Console.ReadLine();
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter mark: ");
+                double mark = double.Parse(Console.ReadLine());
+                students.Add(new Student
+                {
+                    id = id,
+                    name = name,
+                    mark =
+                mark
+                });
+            }
+            Console.WriteLine("\nStudents who passed:");
+            foreach (Student student in students)
+            {
+                if (student.mark >= 50)
+                {
+                    Console.WriteLine(student.name);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
